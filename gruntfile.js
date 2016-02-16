4022ebc5023b9fe8e6910f3f3e0bb2176c73714e
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-16 14:01:10 -0500

    add essential/skeleton code to the gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,14 @@
 module.exports = function(grunt){
+
 	// Configure tasks(s)
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 	});
+
+	// Load the plugins
+	grunt.loadNpmTasks();
+
+	// Register task(s)
+	grunt.registerTask('default', [ ]);
 
 };
